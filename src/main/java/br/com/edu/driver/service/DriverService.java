230c23e3b79593c332f44a46966a7ce4dc6d3f83
cadu <edu.private@gmail.com>
2015-12-11 08:52:23 -0200

    calculando distancias

--- src/main/java/br/com/edu/driver/service/DriverService.java
+++ src/main/java/br/com/edu/driver/service/DriverService.java
@@ -1,7 +1,13 @@
 package br.com.edu.driver.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -49,7 +55,37 @@ public class DriverService {
 		
 		List<Driver> drivers = driverRepository.inArea(swLatitute, neLatitute, swLongitude, neLongitude);
 		
-		return drivers;
+		Double centroidLatitude = (swLatitute - neLatitute) / 2;
+		Double centroidLongitude = (swLongitude - neLongitude) / 2;
+		
+		Map<Double, Driver> distances = new HashMap<Double, Driver>();
+		
+		for (Driver driver : drivers) {
+			distances.put(calculeDistance(centroidLatitude, centroidLongitude, driver.getLatitude(), driver.getLongitude()), driver);
+		}
+		Map<Double, Driver> sortedDistance = new TreeMap<Double, Driver>(distances);
+		
+		List<Driver> sortedDriversList = mountSortedList(sortedDistance);
+		
+		return sortedDriversList;
+	}
+	
+	private List<Driver> mountSortedList(Map<Double,Driver> map) {
+		List<Driver> drivers = new ArrayList<Driver>();
+	    Set<Entry<Double, Driver>> s = map.entrySet();
+	    Iterator<Entry<Double, Driver>> it = s.iterator();
+	    while ( it.hasNext() ) {
+	       Map.Entry entry = (Map.Entry) it.next();
+	       drivers.add((Driver)entry.getValue());  
+	    }
+	    return drivers;
+	}
+	
+	private Double calculeDistance(Double centroidLatitude, Double centroidLongitude, Double latitude, Double longitude ) {
+		Double a = Math.pow(Math.sin(centroidLatitude),2) + Math.cos(latitude) * Math.cos(centroidLatitude) * Math.pow(Math.sin(centroidLatitude),2);  
+        Double distancia = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));  
+        return 6378140 * distancia; 
+		
 	}
 	
 }
